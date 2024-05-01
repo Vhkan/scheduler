a87ea5374ead12d0bc878a357a7e13acd4f7d973
Vhkan <vhkan@yahoo.com>
2024-05-01 13:44:09 -0600

    netlify deploy

---
+++ build/precache-manifest.d8d212c84e2f75ce661649037e996f70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e356b66ebb20ae0f5391d96d82dd916d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fac85d764100503a25e9",
+    "url": "/static/css/2.17e61147.chunk.css"
+  },
+  {
+    "revision": "1cad86880ff2deadd310",
+    "url": "/static/css/main.15c4832a.chunk.css"
+  },
+  {
+    "revision": "fac85d764100503a25e9",
+    "url": "/static/js/2.c4d43e32.chunk.js"
+  },
+  {
+    "revision": "c91cbb924d2e6dd2f7d23f894d2489e8",
+    "url": "/static/js/2.c4d43e32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cad86880ff2deadd310",
+    "url": "/static/js/main.5fdbce91.chunk.js"
+  },
+  {
+    "revision": "243b8f0ab90e65c7feeb",
+    "url": "/static/js/runtime-main.89e9d1db.js"
+  }
+]);
